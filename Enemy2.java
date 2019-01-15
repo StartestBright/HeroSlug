e07d54e2898369799287e976560f9ed6a932fa92
TonyFu <tianhangfu@gmail.com>
2019-01-15 21:20:36 +1300

    MoreEnemy
    add more kinds of enemies

---
+++ Enemy2.java
@@ -0,0 +1,73 @@
+package com.jknull.heroslug;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+public class Enemy2 extends Enemy{
+
+    private static int enemy2MaxHp = 500;
+    public int enemySize=100;
+
+    public Enemy2(Point p,int enemyIndex) {
+        super(p,enemyIndex);
+        curHp = enemy2MaxHp;
+        enemyRect = new Rect(enemyPos.x-enemySize,enemyPos.y-enemySize,enemyPos.x+enemySize,enemyPos.y+enemySize);
+        enemyAlive =true;
+    }
+
+    @Override
+    public String getCharacterTag() {
+        return "Enemy2";
+    }
+
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint p = new Paint();
+        p.setColor(Color.rgb(255,255,255));
+        canvas.drawRect(enemyRect,p);
+    }
+
+
+    @Override
+    public void update() {
+        if(!enemyLanded) {
+            enemyVelocityY += gravity;
+        }else if(enemyLanded){
+            enemyVelocityY = 0;
+            enemyPos.y = MainActivity.SCREEN_HEIGHT-Floor.FLOORHEIGHT-enemySize;
+        }
+        enemyPos.y += enemyVelocityY;
+        enemyPos.x += enemyVelocityX;
+        enemyRect.set(enemyPos.x-enemySize,enemyPos.y-enemySize,enemyPos.x+enemySize,enemyPos.y+enemySize);
+
+
+        //System.out.println(enemyVelocityY);
+
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        curHp -= damage;
+        if(curHp<=0) {
+            EnemyManager.killEnemy(enemyIndex);
+            enemyAlive = false;
+        }
+    }
+
+    @Override
+    public int getEnemySize() {
+        return enemySize;
+    }
+}
---
+++ Enemy3.java
@@ -0,0 +1,88 @@
+package com.jknull.heroslug;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+public class Enemy3 extends Enemy {
+    private static int enemy3MaxHp = 50;
+    public int enemySize = 50;
+
+    public Enemy3(Point p, int enemyIndex) {
+        super(p, enemyIndex);
+        curHp = enemy3MaxHp;
+        enemyRect = new Rect(enemyPos.x - enemySize, enemyPos.y - enemySize, enemyPos.x + enemySize, enemyPos.y + enemySize);
+        enemyAlive = true;
+    }
+
+    @Override
+    public String getCharacterTag() {
+        return "Enemy3";
+    }
+
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint p = new Paint();
+        p.setColor(Color.rgb(200, 200, 200));
+        canvas.drawRect(enemyRect, p);
+    }
+
+
+    @Override
+    public void update() {
+    /*      if(!enemyLanded) {
+         enemyVelocityY += gravity;
+           }else if(enemyLanded) {
+              enemyVelocityY = -500;
+              enemyPos.y = MainActivity.SCREEN_HEIGHT-Floor.FLOORHEIGHT-enemySize;
+          }*/
+        enemyVelocityY = -500;
+        enemyPos.y = MainActivity.SCREEN_HEIGHT-Floor.FLOORHEIGHT-enemySize;
+       if(enemyPos.x == GamePanel.hero.getHeroPos().x) {
+
+
+        }
+
+
+        else if (enemyPos.x < GamePanel.hero.getHeroPos().x) {
+            enemyPos.x += 10;
+        } else if (enemyPos.x > GamePanel.hero.getHeroPos().x) {
+            enemyPos.x -= 10;
+
+
+             }
+            enemyPos.y += enemyVelocityY;
+            enemyPos.x += enemyVelocityX;
+            enemyRect.set(enemyPos.x - enemySize, enemyPos.y - enemySize, enemyPos.x + enemySize, enemyPos.y + enemySize);
+
+
+
+    //System.out.println(enemyVelocityY);
+
+}
+
+    @Override
+    public void takeDamage(int damage) {
+        curHp -= damage;
+        if(curHp<=0) {
+            EnemyManager.killEnemy(enemyIndex);
+            enemyAlive = false;
+        }
+    }
+
+    @Override
+    public int getEnemySize() {
+        return enemySize;
+    }
+}
