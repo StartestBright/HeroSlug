34c4f8d566d2a8f79bf3c96d9653b7fd652da0fb
TonyFu <tianhangfu@gmail.com>
2019-01-27 16:17:00 +1300

    EnemyFixed

--- EnemyGunShot1.java
+++ EnemyGunShot1.java
@@ -16,6 +16,11 @@ public class EnemyGunShot1 extends EnemyGunShot {
     //@RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public EnemyGunShot1(Context context, float velocityX, float velocityY, Point pos, int speed) {
          super(context,velocityX,velocityY,pos,speed);
+
+
+        bulletWidth = 20;
+        bulletHeight = 20;
+        damage = 50;
     }
 
     @Override
