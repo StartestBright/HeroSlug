8b8195ade3318240b68f6aef08db19447148d4aa
junmn <sfd7@naver.com>
2019-01-29 01:15:55 +1300

    drawing bug fixed and rocketman bitmaps and anims are  are added
    and some bug fixed ( rocketman cannot fly when he is landed)

--- RocketMan.java
+++ RocketMan.java
@@ -39,7 +39,7 @@ public class RocketMan extends Hero {
         playerPos = pos;
 
         flyingGaze = FLYINGMAXGAZE;
-        gunShotDelay = 3;
+        gunShotDelay = 80;
         bulletSpeed = rocketSpeed;
         bulletDamage = rocketSpeed;
         playerBullets = new ArrayList<RocketManGunShot>();
@@ -69,62 +69,75 @@ public class RocketMan extends Hero {
         heroMovingRightBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r6);
         heroMovingRightBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r7);
 
-        heroMovingLeftBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r0);
-        heroMovingLeftBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r1);
-        heroMovingLeftBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r2);
-        heroMovingLeftBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r3);
-        heroMovingLeftBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r4);
-        heroMovingLeftBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r5);
-        heroMovingLeftBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r6);
-        heroMovingLeftBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_r7);
+        heroMovingLeftBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l0);
+        heroMovingLeftBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l1);
+        heroMovingLeftBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l2);
+        heroMovingLeftBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l3);
+        heroMovingLeftBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l4);
+        heroMovingLeftBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l5);
+        heroMovingLeftBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l6);
+        heroMovingLeftBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_move_l7);
 
-        heroIdleRightBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle0r);
-        heroIdleRightBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle1r);
-        heroIdleRightBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle2r);
-        heroIdleRightBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle3r);
-        heroIdleRightBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle4r);
-        heroIdleRightBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle5r);
-        heroIdleRightBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle6r);
-        heroIdleRightBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle7r);
-        heroIdleRightBitmaps[8] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle8r);
+        heroIdleRightBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r0);
+        heroIdleRightBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r1);
+        heroIdleRightBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r2);
+        heroIdleRightBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r3);
+        heroIdleRightBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r4);
+        heroIdleRightBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r5);
+        heroIdleRightBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r6);
+        heroIdleRightBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r7);
+        heroIdleRightBitmaps[8] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_r8);
 
-        heroIdleLeftBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle0l);
-        heroIdleLeftBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle1l);
-        heroIdleLeftBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle2l);
-        heroIdleLeftBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle3l);
-        heroIdleLeftBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle4l);
-        heroIdleLeftBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle5l);
-        heroIdleLeftBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle6l);
-        heroIdleLeftBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle7l);
-        heroIdleLeftBitmaps[8] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_idle8l);
+        heroIdleLeftBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l0);
+        heroIdleLeftBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l1);
+        heroIdleLeftBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l2);
+        heroIdleLeftBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l3);
+        heroIdleLeftBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l4);
+        heroIdleLeftBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l5);
+        heroIdleLeftBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l6);
+        heroIdleLeftBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l7);
+        heroIdleLeftBitmaps[8] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_idle_l8);
 
-        heroDyingRightBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r0);
-        heroDyingRightBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r1);
-        heroDyingRightBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r2);
-        heroDyingRightBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r3);
-        heroDyingRightBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r4);
-        heroDyingRightBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r5);
-        heroDyingRightBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r6);
-        heroDyingRightBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r7);
-        heroDyingRightBitmaps[8] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r8);
-        heroDyingRightBitmaps[9] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r9);
-        heroDyingRightBitmaps[10] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r10);
-        heroDyingRightBitmaps[11] = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_r11);
+        heroDyingRightBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r0);
+        heroDyingRightBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r1);
+        heroDyingRightBitmaps[2] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r2);
+        heroDyingRightBitmaps[3] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r3);
+        heroDyingRightBitmaps[4] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r4);
+        heroDyingRightBitmaps[5] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r5);
+        heroDyingRightBitmaps[6] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r6);
+        heroDyingRightBitmaps[7] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r7);
+        heroDyingRightBitmaps[8] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r8);
+        heroDyingRightBitmaps[9] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r9);
+        heroDyingRightBitmaps[10] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r10);
+        heroDyingRightBitmaps[11] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r11);
+        heroDyingRightBitmaps[12] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r12);
+        heroDyingRightBitmaps[13] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r13);
+        heroDyingRightBitmaps[14] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r14);
+        heroDyingRightBitmaps[15] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r15);
+        heroDyingRightBitmaps[16] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r16);
+        heroDyingRightBitmaps[17] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_r17);
 
-        heroDyingLeftBitmaps[0] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l0);
-        heroDyingLeftBitmaps[1] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l1);
-        heroDyingLeftBitmaps[2] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l2);
-        heroDyingLeftBitmaps[3] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l3);
-        heroDyingLeftBitmaps[4] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l4);
-        heroDyingLeftBitmaps[5] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l5);
-        heroDyingLeftBitmaps[6] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l6);
-        heroDyingLeftBitmaps[7] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l7);
-        heroDyingLeftBitmaps[8] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l8);
-        heroDyingLeftBitmaps[9] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l9);
-        heroDyingLeftBitmaps[10] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l10);
-        heroDyingLeftBitmaps[11] =BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_dying_l11);
+        heroDyingLeftBitmaps[0] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l0);
+        heroDyingLeftBitmaps[1] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l1);
+        heroDyingLeftBitmaps[2] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l2);
+        heroDyingLeftBitmaps[3] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l3);
+        heroDyingLeftBitmaps[4] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l4);
+        heroDyingLeftBitmaps[5] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l5);
+        heroDyingLeftBitmaps[6] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l6);
+        heroDyingLeftBitmaps[7] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l7);
+        heroDyingLeftBitmaps[8] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l8);
+        heroDyingLeftBitmaps[9] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l9);
+        heroDyingLeftBitmaps[10] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l10);
+        heroDyingLeftBitmaps[11] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l11);
+        heroDyingLeftBitmaps[12] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l12);
+        heroDyingLeftBitmaps[13] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l13);
+        heroDyingLeftBitmaps[14] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l14);
+        heroDyingLeftBitmaps[15] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l15);
+        heroDyingLeftBitmaps[16] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l16);
+        heroDyingLeftBitmaps[17] =BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_dying_l17);
 
-
+        rocketManFlyingBitmaps[0] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_flying_r);
+        rocketManFlyingBitmaps[1] = BitmapFactory.decodeResource(context.getResources(),R.drawable.rocketman_flying_l);
 
 
 
@@ -133,8 +146,8 @@ public class RocketMan extends Hero {
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void attack() {
-        if(canFire) {
-            RocketManGunShot newBullet = new RocketManGunShot(context, (float) Math.cos(playerRotation), (float) Math.sin(playerRotation), playerPos.x, playerPos.y);
+        if(canFire&&!PlayerHP.HERODEAD) {
+            RocketManGunShot newBullet = new RocketManGunShot(context, (float) Math.cos(playerRotation), (float) Math.sin(playerRotation), getHeroShotSpawnPoint().x, getHeroShotSpawnPoint().y);
             newBullet.setBulletSpeed(bulletSpeed);
             newBullet.setBulletDamage(bulletDamage);
             playerBullets.add(newBullet);
@@ -182,6 +195,7 @@ public class RocketMan extends Hero {
         if(shockShot!=null)
             shockShot.update();
 
+
     }
 
 
@@ -196,12 +210,6 @@ public class RocketMan extends Hero {
         if(shockShot!=null)
             shockShot.draw(canvas);
 
-        super.draw(canvas);
-        for(int i=0;i<playerBullets.size();i++){
-            SoldierGunShot gunShot = (SoldierGunShot)playerBullets.get(i);
-            if(gunShot.isActive())
-                gunShot.draw(canvas);
-        }
 
     }
 
@@ -212,7 +220,7 @@ public class RocketMan extends Hero {
 
     @Override
     public void setSkill1On() {
-        if(!skill1OnCoolTime) {
+        if(!skill1OnCoolTime &&!PlayerHP.HERODEAD &&!playerLanded) {
             super.setSkill1On();
             playerLanded = false;
             playerVelocityY = 0;
@@ -222,11 +230,19 @@ public class RocketMan extends Hero {
 
     @Override
     public void setSkill2On() {
-        if (!skill2OnCoolTime) {
+        if (!skill2OnCoolTime&&!PlayerHP.HERODEAD) {
             super.setSkill2On();
-            shockShot =new RocketManShockShot(context,(float) Math.cos(playerRotation), (float) Math.sin(playerRotation), playerPos.x, playerPos.y);
+            shockShot =new RocketManShockShot(context,(float) Math.cos(playerRotation), (float) Math.sin(playerRotation), getHeroShotSpawnPoint().x, getHeroShotSpawnPoint().y);
         }
     }
+
+    @Override
+    public void setUltimateSkillOn() {
+        if (!skill2OnCoolTime&&!PlayerHP.HERODEAD) {
+            super.setUltimateSkillOn();
+        }
+    }
+
     public void setRockManFlying(boolean flying){
         rockManFlying = false;
     }
