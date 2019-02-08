aacf1024a5c9e3bd7c7cb7f2023f1a0098d0854d
TonyFu <tianhangfu@gmail.com>
2019-02-08 15:28:59 +1300

    not important save

--- EnemyManager.java
+++ EnemyManager.java
@@ -37,7 +37,7 @@ public class EnemyManager {
         this.context = context;
 
 
- //      Boss1(new Point(9000,500));
+       Boss1(new Point(9000,500));
 
       spawnEnemy1(new Point(800,200));
         spawnEnemy1(new Point(1100,100));
@@ -49,11 +49,11 @@ public class EnemyManager {
          spawnEnemy2(new Point(2600,1200));
          spawnEnemy2(new Point(5700,1200));
          spawnEnemy2(new Point(6800,1200));
-  //       spawnEnemy3(new Point(7900,-500));
-   //      spawnEnemy3(new Point(8100,-500));
-   //      spawnEnemy3(new Point(9200,-500));
-   //      spawnEnemy3(new Point(8300,-500));
-   //      spawnEnemy3(new Point(5400,-500));
+         spawnEnemy3(new Point(7900,-500));
+         spawnEnemy3(new Point(8100,-500));
+         spawnEnemy3(new Point(9200,-500));
+         spawnEnemy3(new Point(8300,-500));
+         spawnEnemy3(new Point(5400,-500));
 
         spawnEnemy1(new Point(5800,200));
         spawnEnemy1(new Point(4600,100));
@@ -66,10 +66,10 @@ public class EnemyManager {
         spawnEnemy2(new Point(8200,1200));
         spawnEnemy2(new Point(7300,1200));
         spawnEnemy3(new Point(2000,-500));
-    //    spawnEnemy3(new Point(5500,-500));
-   //     spawnEnemy3(new Point(8600,-500));
-   //     spawnEnemy3(new Point(7700,-500));
-  //      spawnEnemy3(new Point(5800,-500));
+        spawnEnemy3(new Point(5500,-500));
+        spawnEnemy3(new Point(8600,-500));
+        spawnEnemy3(new Point(7700,-500));
+        spawnEnemy3(new Point(5800,-500));
 
         spawnEnemy1(new Point(9900,200));
         spawnEnemy1(new Point(1200,100));
@@ -81,11 +81,11 @@ public class EnemyManager {
         spawnEnemy2(new Point(1700,1200));
         spawnEnemy2(new Point(2800,1200));
         spawnEnemy2(new Point(3100,1200));
-  //      spawnEnemy3(new Point(6200,-500));
-  //      spawnEnemy3(new Point(4300,-500));
-  //      spawnEnemy3(new Point(7400,-500));
-  //      spawnEnemy3(new Point(8500,-500));
-   //     spawnEnemy3(new Point(9600,-500));
+        spawnEnemy3(new Point(6200,-500));
+        spawnEnemy3(new Point(4300,-500));
+        spawnEnemy3(new Point(7400,-500));
+        spawnEnemy3(new Point(8500,-500));
+        spawnEnemy3(new Point(9600,-500));
 
 
 
