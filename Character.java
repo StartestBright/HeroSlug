456854eef4adc2db27c85f72c8e95e03f4acbd0f
TonyFu <tianhangfu@gmail.com>
2019-02-16 02:11:53 +1300

    Finally Fix the git problem

--- Character.java
+++
@@ -1,9 +0,0 @@
-package com.jknull.heroslug;
-
-public interface Character extends GameObject {
-     String getCharacterTag();
-     void jump();
-     void attack();
-     void takeDamage(int damage);
-     float gravity = 9.8f;
-}
