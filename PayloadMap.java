cb26094eb2f411ed25060faca7217c24026d2199
junmn <sfd7@naver.com>
2019-02-16 01:25:51 +1300

    Revert "git repository changed"
    This reverts commit fec696d840d98b472edc8c1ec7de6dba592a395b.

---
+++ PayloadMap.java
@@ -0,0 +1,59 @@
+package com.jknull.heroslug;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.View;
+
+public class PayloadMap extends View {
+    public static int PAYLOADMAPWIDTH= MainActivity.SCREEN_WIDTH/2 ,PAYLOADMAPHEIGHT=50;
+    public static int PAYLOADMAPX=MainActivity.SCREEN_WIDTH/2,PAYLOADMAPY=100;
+
+
+    private static int pointerX,pointerY;
+    private static int pointerSize =25;
+
+
+    public PayloadMap(Context context) {
+        super(context);
+        pointerX = PAYLOADMAPX;
+        pointerY = PAYLOADMAPY-2;
+        pointerX = PAYLOADMAPX;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        Paint paintForMapFrame = new Paint();
+        paintForMapFrame.setStyle(Paint.Style.STROKE);
+        paintForMapFrame.setColor(Color.rgb(132,144,132));
+        paintForMapFrame.setStrokeWidth(5);
+        Path path = new Path();
+        path.moveTo(pointerX,pointerY);
+        path.lineTo(pointerX-pointerSize,pointerY-pointerSize);
+        path.lineTo(pointerX+pointerSize,pointerY-pointerSize);
+        path.lineTo(pointerX,pointerY);
+        path.close();
+        Paint paintForPointer = new Paint();
+        paintForPointer.setStyle(Paint.Style.FILL);
+        paintForPointer.setColor(Color.BLUE);
+        canvas.drawPath(path,paintForPointer);
+        canvas.drawRect(PAYLOADMAPX,PAYLOADMAPY,PAYLOADMAPX+PAYLOADMAPWIDTH,PAYLOADMAPY+PAYLOADMAPHEIGHT,paintForMapFrame);
+
+        Paint paintForMapFill = new Paint();
+        paintForMapFill.setColor(Color.rgb(132,144,132));
+        paintForMapFill.setStyle(Paint.Style.FILL);
+        canvas.drawRect(PAYLOADMAPX,PAYLOADMAPY,pointerX,PAYLOADMAPY+PAYLOADMAPHEIGHT,paintForMapFill);
+
+    }
+
+    public static void movePayloadPointer(float ratio){
+        pointerX = (int) (PAYLOADMAPX + (PAYLOADMAPWIDTH*ratio));
+
+    }
+
+
+}
---
+++ SoldierHealPack.java
@@ -0,0 +1,85 @@
+package com.jknull.heroslug;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class SoldierHealPack implements GameObject{
+    Context context;
+    private Point healpackPos;
+    private int healpackRange = 80;
+    private int healpackHeight = 80;
+    private float healpackGravity = 9.8f;
+    private boolean healpackLanded = false;
+    private float healpackVelocityY = 1;
+    private Bitmap healPackBitmap;
+
+    private boolean healpackOn = true;
+    private long healpackStartTime;
+
+
+    Rect healpack;
+    Hero hero;
+
+
+    long start,end;
+    public SoldierHealPack(Point point,Hero hero,Context context){
+        this.context = context;
+        healPackBitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.soldier_healpack_img);
+        healpackPos = new Point(point.x,point.y);
+
+        this.hero = hero;
+        healpack = new Rect(healpackPos.x,healpackPos.y,healpackPos.x,healpackPos.y);
+        //healpack = new Rect(-10,-10,10,10);
+        healpack.set(healpackPos.x-healpackRange,healpackPos.y-healpackHeight,healpackPos.x+healpackRange,healpackPos.y+healpackHeight);
+
+        healpackOn = true;
+        healpackStartTime = System.currentTimeMillis();
+
+
+    }
+    @Override
+    public void draw(Canvas canvas) {
+            Paint p = new Paint();
+            p.setColor(Color.YELLOW);
+            //canvas.drawRect(healpack, p);
+            canvas.drawBitmap(healPackBitmap,null,healpack,null);
+
+
+    }
+
+    @Override
+    public void update() {
+        Rect tempHero = hero.getHero();
+        if(!healpackLanded)
+            healpackVelocityY = healpackGravity*1;
+        if(healpack.bottom>=GamePanel.floorRect.top && healpack.top<=GamePanel.floorRect.bottom && healpack.left<=GamePanel.floorRect.right&&healpack.right>=GamePanel.floorRect.left){
+            healpackVelocityY = 0f;
+            healpackLanded = true;
+        }
+        healpackPos.y += healpackVelocityY;
+        healpack.set(healpackPos.x-healpackRange,healpackPos.y-healpackHeight,healpackPos.x+healpackRange,healpackPos.y+healpackHeight);
+        if((System.currentTimeMillis()-healpackStartTime)/1000<=5){
+            if(tempHero.right>=healpack.left&&tempHero.left<=healpack.right &&tempHero.top<=healpack.bottom &&tempHero.bottom>=healpack.top) {
+                GamePanel.HEROHP.getHeal(1);
+            }
+        }else{
+            Soldier soldier = (Soldier) hero;
+            soldier.setHealPackNull();
+        }
+
+    }
+
+    public void moveByHero(float heroVelocityX){
+        healpackPos.x -= heroVelocityX;
+    }
+
+}
